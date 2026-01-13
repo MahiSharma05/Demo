7fbcc4c8707304abb88280f05471fcb87a53abce
MahiSharma05 <sharmama0905@gmail.com>
2026-01-13 15:58:16 +0530

    [Mahi Sharma] Commited.

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,9 @@
-public class Calculator{}
+public class Calculator{
+  public static int add(int a , int b){
+		return a+b;
+	}
+    public static void main(String[] args){
+		int result = add(4,7);
+		System.out.println(result);
+	}
+}
