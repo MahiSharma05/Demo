998cc028a95b8a3a539d4d57796f703db6a98373
MahiSharma05 <sharmama0905@gmail.com>
2026-01-13 16:36:02 +0530

    [Mahi Sharma] Division updated.

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,15 @@
 public class Calculator{
-  public static int addition2(int a , int b){
+  public static int addition(int a , int b){
 	  return a+b;
   }
-    
-    public static int addition(int a , int b){
-        return a+b;
-    }
-	
-	public static int add(int a, int b) {
-		return a+b;
+	public static int div(int a, int b) {
+		return a/b;
 	}
-    /*public static void main(String[] args){
-        int result = add(5,6);
-        System.out.println(result);
-    }*/
-	
-    /*public static void main(String[] args){
-		int result = add(4,7);
-	public static int add(int a, int b) {
-		return a+b;
-	}*/
 	
 	public static void main(String[] args) {
-		int result = add(5, 4);
-		System.out.println(result);
-		int result2 = addition(5,6);
+		int result1 = addition(4,6);
+        System.out.println(result1);
+		int result2 = div(4,2);
         System.out.println(result2);
-		int result3 = addition2(4,6);
-        System.out.println(result3);
 	}
 }
