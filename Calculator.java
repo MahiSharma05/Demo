33b634cabe6b3d4e69752b97d43590a5dabbf2e6
Rahul Agrawal <agrawalrahul11112004@gmail.com>
2026-01-13 16:24:36 +0530

    [Rahul Agrawal] Add subtract method.

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,30 @@
 public class Calculator{
-  public static int addition2(int a , int b){
+  /* public static int addition2(int a , int b){
 	  return a+b;
   }
     
     public static int addition(int a , int b){
         return a+b;
     }
+	*/ 
 	
 	public static int add(int a, int b) {
 		return a+b;
 	}
-    /*public static void main(String[] args){
-        int result = add(5,6);
-        System.out.println(result);
-    }*/
 	
-    /*public static void main(String[] args){
-		int result = add(4,7);
-	public static int add(int a, int b) {
-		return a+b;
-	}*/
+	public static int subtract(int a, int b) {
+		return a-b;
+	}
 	
 	public static void main(String[] args) {
-		int result = add(5, 4);
-		System.out.println(result);
-		int result2 = addition(5,6);
+		int addResult = add(5, 4);
+		System.out.println(addResult);
+		/* int result2 = addition(5,6);
         System.out.println(result2);
 		int result3 = addition2(4,6);
-        System.out.println(result3);
+        System.out.println(result3); */
+		
+		int subtractResult = subtract(15, 4);
+		System.out.println(subtractResult);
 	}
 }
