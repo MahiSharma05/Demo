f0b1048e35fb1a7fbcd4a30e02ecf311e86dcea8
deepakushwaha17 <deepakushwaha155@gmail.com>
2026-01-13 16:24:56 +0530

    mul

--- Calculator.java
+++ Calculator.java
@@ -1,21 +1,12 @@
 public class Calculator{
     
-    public static int addition(int a , int b){
-        return a+b;
-    }
-    /*public static void main(String[] args){
-        int result = add(5,6);
-        System.out.println(result);
-    }*/
-	public static int add(int a, int b) {
-		return a+b;
+	public static int multiply(int a, int b) {
+		return a*b;
 	}
 	
 	public static void main(String[] args) {
-		int result = add(5, 4);
+		int result = multiply(5, 4);
 		System.out.println(result);
-		int result2 = addition(5,6);
-        System.out.println(result2);
 		
 	}
 }
