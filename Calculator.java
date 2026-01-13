4dfe908152138f4afbe2dabb20adc53f3f7f9693
deepakushwaha17 <deepakushwaha155@gmail.com>
2026-01-13 16:47:40 +0530

    mul

--- Calculator.java
+++ Calculator.java
@@ -7,11 +7,18 @@ public class Calculator{
 	public static int add(int a, int b) {
 		return a+b;
 	}
+	
+	public static int divide(int a, int b) {
+		return a/b;
+	}
 	public static void main(String[] args) {
 		int result = multiply(5, 4);
 		System.out.println(result);
 		
 		int result1 = add(10, 5);
 		System.out.println(result1);
+		
+		int result2 = divide(10, 5);
+		System.out.println(result2);
 	}
 }
