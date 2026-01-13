d572da33d25a6f8255d0a7230e07eaf4779a4f2b
Rahul Agrawal <agrawalrahul11112004@gmail.com>
2026-01-13 15:55:54 +0530

    [Rahul Agrwal] created an add method.

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,10 @@
-public class Calculator{}
+public class Calculator{
+	public static int add(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		int result = add(5, 4);
+		System.out.println(result);
+	}
+}
